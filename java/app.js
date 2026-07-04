5c4941c21d846a91ea73aefbcf5a5ec52c74607a
Yehashi <karavitavidanalagedonyehashi@gmail.com>
2026-07-04 13:04:59 +0530

    2

--- java/app.js
+++ java/app.js
@@ -1,78 +1,121 @@
-class customer{
-name;
-age;
-address; 
+// class customer{
+// name;
+// age;
+// address; 
 
-constructor(name,age,address){
-    this.name = name;
-    this.age = age;
-    this.address = address;
+// constructor(name,age,address){
+//     this.name = name;
+//     this.age = age;
+//     this.address = address;
 
-    }
+//     }
+// }
+
+// let customer1 = new customer("Yeha", 20 , "kaluthara");
+// console.log(customer1);
+
+
+
+// let customer2= {
+//     name: "Kashmika",
+//     age: 19,
+//     address: "panadura",
+//     salary:70000,
+//      items: [
+//         {
+//             id: 1,
+//             name: "item1",
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 10
+//                 },
+//                 {
+//                     id: 2,
+//                     name: "variant2",
+//                     price: 20
+                    
+//                 }
+//             ]
+//         }, {
+//             id: 2,
+//             name: "item2",
+//             price: 200
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300
+//         }
+//     ],
+
+
+
+
+//     father:{
+//         name: "nishantha",
+//         age: 47,
+//         address: "panadura"
+//     }
+// };
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items[0].variants[1].id);
+// console.log(customer2.father.name);
+// console.log(customer2.father.age);
+// console.log(customer2.father.address);
+
+
+
+
+// let headline = document.getElementById("headline");
+
+// // headline.innerText = "yehashi";
+
+// // console.log(headline);
+// let count = 0
+// function up(){
+// count++
+// headline.innerText = "yehashi"+ count;
+// console.log("clicked")
+// }
+
+
+//  document.write("<h2> hello world </h2>");
+
+
+//  console.log(document.title);
+
+// function down(){
+// count--
+// headline.innerText = "yehashi"+ count;
+// console.log("clicked")
+// }
+
+
+function sum() {
+    let add1 =Number(document.getElementById("txt1")) ;
+    let add2 =Number(document.getElementById("txt2"))
+    let collect = add1 + add2;
+
+    ew.innertext = collect.value
+    console.log(collect.value);
+    
 }
 
-let customer1 = new customer("Yeha", 20 , "kaluthara");
-console.log(customer1);
 
+function red() {
+    let red1 =Number(document.getElementById("txt1")) ;
+    let red2 =Number(document.getElementById("txt2"))
+    let collect = red1 + red2;
 
-
-let customer2= {
-    name: "Kashmika",
-    age: 19,
-    address: "panadura",
-    salary:70000,
-     items: [
-        {
-            id: 1,
-            name: "item1",
-            price: 100,
-            variants: [
-                {
-                    id: 1,
-                    name: "variant1",
-                    price: 10
-                },
-                {
-                    id: 2,
-                    name: "variant2",
-                    price: 20
-                    
-                }
-            ]
-        }, {
-            id: 2,
-            name: "item2",
-            price: 200
-        }, {
-            id: 3,
-            name: "item3",
-            price: 300
-        }
-    ],
-
-
-
-
-    father:{
-        name: "nishantha",
-        age: 47,
-        address: "panadura"
-    }
-};
-console.log(customer2.name);
-console.log(customer2.age);
-console.log(customer2.address);
-console.log(customer2.salary);
-console.log(customer2.items[0].variants[1].id);
-console.log(customer2.father.name);
-console.log(customer2.father.age);
-console.log(customer2.father.address);
-
-
-
-
-
-
+    ew.innertext = collect.value
+    console.log(collect.value);
+    
+}
 
 
 
